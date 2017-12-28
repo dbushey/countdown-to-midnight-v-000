51553641badc98bebb68bea183f0089e83467397
Daiane Bushey <daianebushey@gmail.com>
2017-12-28 22:40:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -14,7 +14,7 @@ def countdown_with_sleep(int)
   while int > 0
     puts "#{int} SECOND(S)!"
     int -= 1
-    sleep(0.5) #Half a second
+    sleep(0.6) #Half a second
   end
   return "HAPPY NEW YEAR!"
 end
